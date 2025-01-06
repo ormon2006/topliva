e8583f4511cad4f43c96b3f4531e9c8c60a3cf73
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2025-01-06 23:33:53 +0600

    responsive design

--- src/pages/dashboard/dashboard-page.ui.tsx
+++ src/pages/dashboard/dashboard-page.ui.tsx
@@ -2,6 +2,9 @@ import { Typography, Paper, LinearProgress, Box } from '@mui/material';
 import { articleQueries } from '~entities/makalabox';
 import { ArticleCard } from '~entities/makalabox/ui/ArticleCard';
 import { userQueries } from '~entities/user';
+import { BadgeList } from '~widgets/badge-list';
+import { GoalList } from '~widgets/goal-list';
+import { MakalaList } from '~widgets/makala-list';
 import { StudentsRanking } from '~widgets/students-ranking';
 
 const usersData = {
@@ -9,10 +12,6 @@ const usersData = {
     { name: 'Айдар', points: 1500 },
     { name: 'Камила', points: 1400 },
     { name: 'Максат', points: 1200 },
-  ],
-  goals: [
-    { id: 1, title: 'Завершить 10 домашних заданий', progress: 60 },
-    { id: 2, title: 'Достигнуть 6 уровня', progress: 50 },
   ],
 };
 
@@ -22,9 +21,6 @@ export function DashboardPage() {
     isLoading,
     isError,
   } = userQueries.useLoginUserQuery();
-  const {
-    data: articles, isSuccess
-  } = articleQueries.useGetArticles()
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -33,8 +29,6 @@ export function DashboardPage() {
   if (isError) {
     return <div>Error fetching user data.</div>;
   }
-
-  const latestArticles = articles?.data.results.slice(0, 3);
 
   return (
     <div className="my-10 flex flex-col gap-6">
@@ -52,54 +46,9 @@ export function DashboardPage() {
         </Typography>
         <StudentsRanking />
       </Paper>
-      <Paper elevation={3} className="p-5 shadow-none border border-alto">
-        <Typography variant="h6" className="font-bold mb-3">
-          Последние достижения
-        </Typography>
-        <div className="flex gap-4">
-          {userData.data.achievements.map((ach) => (
-            <div key={ach.id} className="flex flex-col items-center">
-              <img src={ach.photo} alt={ach.name} className="w-16 h-16" />
-              <Typography variant="body2" className="text-center">
-                {ach.name}
-              </Typography>
-            </div>
-          ))}
-        </div>
-      </Paper>
-      <Paper
-        elevation={3}
-        className="relative p-5 shadow-none border border-alto rounded-md"
-      >
-        <Box className="absolute inset-0 bg-black/70 flex items-center justify-center z-10 rounded-md">
-          <Typography variant="h6" className="text-white font-bold text-center">
-            Скоро <br />
-            (В разработке)
-          </Typography>
-        </Box>
-        <Typography variant="h6" className="font-bold mb-3">
-          Прогресс целей
-        </Typography>
-        {usersData.goals.map((goal) => (
-          <div key={goal.id} className="mb-4">
-            <Typography variant="body2">{goal.title}</Typography>
-            <LinearProgress variant="determinate" value={goal.progress} />
-            <Typography variant="caption">{goal.progress}%</Typography>
-          </div>
-        ))}
-      </Paper>
-      <Paper
-        elevation={3}
-        className=" p-5 shadow-none border border-alto rounded-md flex flex-col items-center gap-5"
-      >
-        <Typography variant="h6" className="font-bold ">
-          Статьи на Makalabox
-        </Typography>
-        {isSuccess && latestArticles.map((article) => (
-          <ArticleCard article={article} key={article.id} />
-        ))}
-      </Paper>
-
+      <BadgeList achievements={userData.data.achievements} />
+      <GoalList />
+      <MakalaList />
     </div>
   );
 }
